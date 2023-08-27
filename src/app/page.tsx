0eb686f87e227cb7f4e8b318368131e3d9504413
Asomiddin <asomiddin320@gmail.com>
2023-08-27 16:07:54 -0700

    ggg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ console.log(products);
   return (
    <main className=' min-h-screen max-w-7xl mx-auto px-8 xl:px-0 '>
     <NextJsCarousel />
+    
     <section className='flex flex-col space-y-12'>
       <h1 className='text-5xl font-bold text-center '>Acoder-Market shop deals</h1>
       <div className=' grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 
